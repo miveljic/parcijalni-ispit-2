5d9e1b312177b54acacd14d1422744e24a17c4f5
Miljenko Iveljić <117017083+miveljic@users.noreply.github.com>
2024-03-11 16:34:08 +0100

    Add files via upload

---
+++ Ispit.Konzola/PametniTelefon.cs
@@ -0,0 +1,62 @@
+﻿using Ispit.Konzola.Sucelja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    public class PametniTelefon : IPametniTelefon
+    {
+        public string Model {  get; private set; }
+        public PametniTelefon(string model)
+        {
+            Model = model;
+        }
+        public string Surfanje(string url)
+        {
+            if (ProvjeriAkoJeValidanUrl(url))
+            {
+                return "Moguce je surfati na " + url;
+            }
+            else
+            {
+                return "Neispravan url";
+            }
+        }
+        public string Poziv(string telefonski_broj)
+        {
+            if (ProvjeriAkoJeValidanBroj(telefonski_broj))
+            {
+                return "Moguce je zvati broj " + telefonski_broj;
+            }
+            else
+            {
+                return "Neispravan telefonski broj!";
+            }
+        }
+        static bool ProvjeriAkoJeValidanBroj(string telefonski_broj)
+        {
+            foreach (char c in telefonski_broj)
+            {
+                if (!char.IsDigit(c))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        static bool ProvjeriAkoJeValidanUrl(string url)
+        {
+            foreach (char u in url)
+            {
+                if (char.IsDigit(u))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
---
+++ Ispit.Konzola/Program.cs
@@ -0,0 +1,32 @@
+﻿using Ispit.Konzola.Sucelja;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ispit.Konzola
+{
+    internal class Program
+    {
+        public static void TestirajTelefon(IPametniTelefon telefon)
+        {
+            Console.Write("Unesite broj telefona: ");
+            string telefonski_broj = Console.ReadLine();
+            Console.WriteLine(telefon.Poziv(telefonski_broj));
+
+            Console.Write("Unesite url: ");
+            string url = Console.ReadLine();
+            Console.WriteLine(telefon.Surfanje(url));
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                PametniTelefon telefon = new PametniTelefon("IPhone");
+                TestirajTelefon(telefon);
+                Console.WriteLine();
+            }
+        }
+    }
+}
